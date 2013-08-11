1a160cf283641ed52a18551cc5d52714839eb475
Ruthie Nachmany <ruthienachmany@gmail.com>
2013-08-11 15:51:40 -0400

    more versions

--- lib/omniauth-jawbone/version.rb
+++ lib/omniauth-jawbone/version.rb
@@ -1,5 +1,5 @@
 module OmniAuth
   module Jawbone
-    VERSION = "0.0.1"
+    VERSION = "0.0.7"
   end
 end
--- lib/omniauth/strategies/jawbone.rb
+++ lib/omniauth/strategies/jawbone.rb
@@ -12,13 +12,16 @@ module OmniAuth
     # You must generally register your application with the provider and
     # utilize an application id and secret in order to authenticate using
     # OAuth 2.0.
-    class Jawbone < OmniAuth::Strategy
+    class Jawbone
+      include OmniAuth::Strategy
       option :client_options, {
         :site => 'https://jawbone.com',
         :authorize_url => '/auth/oauth2/auth',
         :token_url => '/auth/oauth2/auth/token'
       }
-      
+      option :authorize_params, {}
+      option :authorize_options, [:scope]
+      option :auth_token_params, {}
 
       def request_phase
         super
@@ -29,29 +32,13 @@ module OmniAuth
       info do
         {
           'basic_read' => user_data['basic_read']
-          'extended_read' => user_data['extended_read']
-          'location_read' => user_data['location_read']
-          'friends_read' => user_data['friends_read']
-          'mood_read' => user_data['mood_read']
-          'mood_write' => user_data['mood_write']
-          'move_read' => user_data['move_read']
-          'move_write' => user_data['move_write']
-          'sleep_read' => user_data['sleep_read']
-          'meal_read' => user_data['meal_read']
-          'meal_write' => user_data['meal_write']
-          'weight_read' => user_data['weight_read']
-          'weight_write' => user_data['weight_write']
-          'cardiac_read' => user_data['cardiac_read']
-          'cardiac_write' => user_data['cardiac_write']
-          'generic_event_read' => user_data['generic_event_read']
-          'generic_event_write' => user_data['generic_event_write']
-        },
+        }
       end
 
     
       def user_data
         access_token.options[:mode] = :query
-        user_data ||= access_token.get('/1/me').parsed
+        user_data ||= access_token.get('/nudge/api/users/@me').parsed
       end
 
     end
