e6cf371f2bae918c2a4eadb8015722bd8337df65
Ruthie Nachmany <ruthienachmany@gmail.com>
2013-08-16 11:10:52 -0400

    updated version

--- lib/omniauth-jawbone/version.rb
+++ lib/omniauth-jawbone/version.rb
@@ -1,5 +1,5 @@
 module OmniAuth
   module Jawbone
-    VERSION = "0.0.34"
+    VERSION = "0.0.35"
   end
 end
--- lib/omniauth/strategies/jawbone.rb
+++ lib/omniauth/strategies/jawbone.rb
@@ -27,15 +27,15 @@ module OmniAuth
       end
 
 
-      def authorize_params
-        super.tap do |params|
-          %w[scope client_options].each do |v|
-            if request.params[v]
-              params[v.to_sym] = request.params[v]
-            end
-          end
-        end
-      end
+      # def authorize_params
+      #   super.tap do |params|
+      #     %w[scope client_options].each do |v|
+      #       if request.params[v]
+      #         params[v.to_sym] = request.params[v]
+      #       end
+      #     end
+      #   end
+      # end
 
       uid { raw_info['id'].to_s }
 
@@ -52,10 +52,10 @@ module OmniAuth
         {:raw_info => raw_info}
       end
 
-      def raw_info
-        access_token.options[:mode] = :query
-        @raw_info ||= access_token.get('user').parsed
-      end
+      # def raw_info
+      #   access_token.options[:mode] = :query
+      #   @raw_info ||= access_token.get('user').parsed
+      # end
 
 
       def user_data
